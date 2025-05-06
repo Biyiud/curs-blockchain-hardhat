50fe66b59250e2b1c2e76e394112a27cff94ce67
Jaume Catarineu <j.catarineu@ubiquat.com>
2025-05-06 23:41:56 +0200

    Only 5 addresses

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,12 @@ require("@nomicfoundation/hardhat-ethers"); // Required for using ethers.js with
 module.exports = {
   solidity: "0.8.20",
   networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        count: 5, // Generate only 5 accounts
+      },
+    },
     localhost: {
       url: "http://127.0.0.1:8545/",
     },
